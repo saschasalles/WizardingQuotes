a9a7b38c6e4909bc81577d0e618d301a0eb24fea
Sascha Sallès <sascha.salles@ynov.com>
2021-03-02 23:05:18 +0100

    Initial Commit

---
+++ WizardingQuotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WizardingQuotes
+//
+//  Created by Sascha Sallès on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WizardingQuotes/WizardingQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardingQuotesApp.swift
+//  WizardingQuotes
+//
+//  Created by Sascha Sallès on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WizardingQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
